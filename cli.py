0574fd4be5b28dbcdea6f729b49147c1ab032a78
SeanMcKenney <47224579+SeanMcKenney@users.noreply.github.com>
2019-04-04 13:28:52 -0500

    CLI test of factorial

--- cli.py
+++ cli.py
@@ -2,6 +2,10 @@
     Usage:
         cli.py add <x> <y>
         cli.py subtract <x> <y>
+        cli.py factorial <x>
+        cli.py fibonacci <x>
+        cli.py is-prime <x>
+        cli.py slack-alert <string>
 """
 
 # import docopt (remember to install)
@@ -16,7 +20,21 @@ def run():
     if args['subtract']:
         x = float(args['<x>'])
         y = float(args['<y>'])
-        print(x-y)        
+        print(x-y) 
+    if args['factorial']:
+        num = int(args['<x>'])
+        try:
+            use = int(num)
+            total = int(num)
+            for i in range(1, int(num) - 1):
+                use -= 1
+                total = total * use
+            print(int(num), total)
+        #return str(total)
+        except ValueError:
+            print(num, "Input is not a positive integer")
+
+       
     
 if __name__ == "__main__":
     
